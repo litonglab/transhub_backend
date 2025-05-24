e44efdb75fea165a50d8505906d950e9d1ebe255
ggbondzhu <878038720@qq.com>
2025-05-24 10:52:27 +0800

    增加前端随后端一起打包功能

--- app_backend/__init__.py
+++ app_backend/__init__.py
@@ -1,5 +1,6 @@
+import os
 from datetime import datetime
-from flask import Flask
+from flask import Flask, render_template
 from flask_cors import CORS
 from flask_redis import FlaskRedis
 # from flask_rq2 import RQ
@@ -8,6 +9,7 @@ from flask_sqlalchemy import SQLAlchemy
 # from dramatiq.brokers.redis import RedisBroker
 # from dramatiq.middleware import TimeLimit
 import time
+
 redis_client = FlaskRedis()
 # rq = RQ()
 
@@ -32,11 +34,29 @@ def make_dir():
 
 def get_app():
     st = time.time()
-    app = Flask(__name__)
+
+    dist_path = 'app_frontend_dist/dist'
+    dist_path = os.path.abspath(dist_path)
+    print(f"frontend dist path: {dist_path}")
+    # 判断dist_path是否存在，用于判断前端是否随后端一起打包
+    if not os.path.exists(dist_path):
+        print("前端dist文件夹不存在，请单独部署前端")
+        app = Flask(__name__)
+    else:
+        print("前端dist文件夹存在，前端随后端一同部署")
+        app = Flask(__name__,
+                    static_folder=dist_path + '/assets', template_folder=dist_path)
+
+        # 未定义路由重定向到前端，由前端处理
+        @app.errorhandler(404)
+        def index(args):
+            # logger.info('404 handler, args: ' + str(args))
+            return render_template("index.html")
     CORS(app, supports_credentials=True, origins='*')
+
     app.config.from_object('app_backend.config')
     redis_client.init_app(app)
-    #rq.init_app(app)
+    # rq.init_app(app)
 
     app.secret_key = str(hash(str(datetime.now())))
     app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://' + app.config['MYSQL_USERNAME'] + ':' + app.config[
@@ -80,6 +100,6 @@ def create_app():
         app.register_blueprint(source_code_bp)
         app.register_blueprint(graph_bp)
         end_time = time.time()
-        print(f"App context loaded in {end_time- start_time} seconds")
+        print(f"App context loaded in {end_time - start_time} seconds")
 
     return app
